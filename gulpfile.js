a35d9f7a2648aa870c571d540e8533a9cc3c971e
mikewolfd <michael.f.deeb@gmail.com>
2025-07-07 20:57:17 -0400

    feat: added hashed css filesto address caching issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 import gulp from "gulp";
-import imagemin, { gifsicle, svgo } from "gulp-imagemin";
+import imagemin, {gifsicle, svgo} from "gulp-imagemin";
 import sharpResponsive from "gulp-sharp-responsive";
 import uswds from "@uswds/compile";
+import rev from "gulp-rev";
+import revDelete from "gulp-rev-delete-original";
+
 
 /**
  * USWDS version
@@ -9,11 +12,14 @@ import uswds from "@uswds/compile";
 
 uswds.settings.version = 3;
 
+
 /**
  * Configure USWDS source paths
  */
 uswds.paths.src.projectSass = "./sass/custom";
+
 // uswds.paths.src.theme = "./sass/theme";
+
 
 /**
  * Configure USWDS destination paths
@@ -24,6 +30,22 @@ uswds.paths.dist.js = "./assets/js";
 uswds.paths.dist.fonts = "./assets/fonts";
 uswds.paths.dist.img = "./assets/img";
 
+
+/**
+ * CSS compilation with cache busting
+ */
+
+
+function addHashToCSS() {
+  return gulp
+    .src("./assets/css/{styles,events,civichackdc}.css") // Process multiple CSS files
+    .pipe(rev()) // Add hash to filename
+    .pipe(gulp.dest("./assets/css"))
+    .pipe(rev.manifest('css-manifest.json')) // Create manifest
+    .pipe(gulp.dest('./_data')); // Save manifest to Jekyll data folder
+}
+
+
 /**
  * Image processing tasks
  */
@@ -32,68 +54,80 @@ const imageDest = "./assets/images";
 
 // Resizes and optimizes raster images
 function resizeImages() {
-  return gulp
-    .src("_images/**/*.{jpg,jpeg,png}", { encoding: false })
-    .pipe(
-      sharpResponsive({
-        formats: [
-          // For each original format, create different sizes
-          {
-            width: 400,
-            rename: { suffix: "-400w" },
-            jpegOptions: { quality: 75, progressive: true },
-            withoutEnlargement: true,
-          },
-          {
-            width: 800,
-            rename: { suffix: "-800w" },
-            jpegOptions: { quality: 75, progressive: true },
-            withoutEnlargement: true,
-          },
-          {
-            width: 1200,
-            rename: { suffix: "-1200w" },
-            jpegOptions: { quality: 80, progressive: true },
-            withoutEnlargement: true,
-          },
-          {
-            width: 1800,
-            rename: { suffix: "-1800w" },
-            jpegOptions: { quality: 90, progressive: true },
-            withoutEnlargement: true,
-          },
-        ],
-      })
-    )
-    .pipe(gulp.dest(imageDest));
+  return gulp.src("_images/**/*.{jpg,jpeg,png}", {encoding: false}).pipe(sharpResponsive({
+    formats: [
+
+      // For each original format, create different sizes
+      {
+        width: 400,
+        rename: {
+          suffix: "-400w"
+        },
+        jpegOptions: {
+          quality: 75,
+          progressive: true
+        },
+        withoutEnlargement: true
+      }, {
+        width: 800,
+        rename: {
+          suffix: "-800w"
+        },
+        jpegOptions: {
+          quality: 75,
+          progressive: true
+        },
+        withoutEnlargement: true
+      }, {
+        width: 1200,
+        rename: {
+          suffix: "-1200w"
+        },
+        jpegOptions: {
+          quality: 80,
+          progressive: true
+        },
+        withoutEnlargement: true
+      }, {
+        width: 1800,
+        rename: {
+          suffix: "-1800w"
+        },
+        jpegOptions: {
+          quality: 90,
+          progressive: true
+        },
+        withoutEnlargement: true
+      }
+    ]
+  })).pipe(gulp.dest(imageDest));
 }
 
 // Optimizes vector images and GIFs
 function optimizeSvgsAndGifs() {
-  return gulp
-    .src("_images/**/*.{svg,gif}", { encoding: false })
-    .pipe(
-      imagemin(
-        [
-          gifsicle({ interlaced: true }),
-          svgo({
-            plugins: [
-              { name: "removeViewBox", active: false },
-              { name: "cleanupIDs", active: false },
-            ],
-          }),
-        ],
-        { verbose: true }
-      )
+  return gulp.src("_images/**/*.{svg,gif}", {encoding: false}).pipe(imagemin([
+    gifsicle(
+      {interlaced: true}
+    ),
+    svgo(
+      {
+        plugins: [
+          {
+            name: "removeViewBox",
+            active: false
+          }, {
+            name: "cleanupIDs",
+            active: false
+          }
+        ]
+      }
     )
-    .pipe(gulp.dest(imageDest));
+  ], {verbose: true})).pipe(gulp.dest(imageDest));
 }
 
 // Copies WebP files without processing
 function copyWebpFiles() {
-  return gulp
-    .src("_images/**/*.webp", { encoding: false })
-    .pipe(gulp.dest(imageDest));
+  return gulp.src("_images/**/*.webp", {encoding: false}).pipe(gulp.dest(imageDest));
 }
 
 const processImages = gulp.parallel(resizeImages, optimizeSvgsAndGifs, copyWebpFiles);
@@ -102,11 +136,12 @@ function watchImages() {
   return gulp.watch(imageSource, processImages);
 }
 
+
 /**
  * Functions exported as gulp tasks
  */
 
-export const compile = gulp.series(processImages, uswds.compile);
+export const compile = gulp.series(processImages, uswds.compile, addHashToCSS);
 const watchUsdws = (done) => uswds.watch(done);
 
 export const watch = gulp.parallel(watchUsdws, watchImages);
